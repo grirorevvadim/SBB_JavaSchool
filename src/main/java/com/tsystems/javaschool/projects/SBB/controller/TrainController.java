50414d49040fbff5311a26132fa48ba632f61679
vgrigory <grigoryevvadim@mail.ru>
2021-10-10 21:02:01 +0300

    Train controller is updated

--- src/main/java/com/tsystems/javaschool/projects/SBB/controller/TrainController.java
+++ src/main/java/com/tsystems/javaschool/projects/SBB/controller/TrainController.java
@@ -1,16 +1,14 @@
 package com.tsystems.javaschool.projects.SBB.controller;
 
-import com.tsystems.javaschool.projects.SBB.service.TrainService;
 import com.tsystems.javaschool.projects.SBB.domain.dto.TrainDTO;
-import com.tsystems.javaschool.projects.SBB.service.util.response.OperationStatus;
-import com.tsystems.javaschool.projects.SBB.ui.models.request.TrainDetailsModel;
+import com.tsystems.javaschool.projects.SBB.service.TrainService;
 import com.tsystems.javaschool.projects.SBB.service.util.response.OperationName;
-import com.tsystems.javaschool.projects.SBB.ui.models.response.TrainRest;
-import org.springframework.beans.BeanUtils;
+import com.tsystems.javaschool.projects.SBB.service.util.response.OperationStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("trains")
 public class TrainController {
 
@@ -18,32 +16,24 @@ public class TrainController {
     TrainService trainService;
 
     @GetMapping(path = "/{id}")
-    public TrainRest getTrain(@PathVariable String id) {
-        TrainRest trainRest = new TrainRest();
-        TrainDTO trainDTO = trainService.getTrainByTrainId(id);
-        BeanUtils.copyProperties(trainDTO, trainRest);
-        return trainRest;
+    public TrainDTO getTrain(@PathVariable String id) {
+        return trainService.getTrainByTrainId(id);
     }
 
     @PostMapping
-    public TrainRest postTrain(@RequestBody TrainDetailsModel trainDetails) {
-        TrainRest trainRest = new TrainRest();
-        TrainDTO trainDTO = new TrainDTO();
-        BeanUtils.copyProperties(trainDetails, trainDTO);
-        TrainDTO createdTrain = trainService.createTrain(trainDTO);
-        BeanUtils.copyProperties(createdTrain, trainRest);
-        return trainRest;
+    public TrainDTO postTrain(@ModelAttribute(name = "train") TrainDTO trainDTO) {
+        return trainService.createTrain(trainDTO);
     }
 
-    @PutMapping(path = "/{id}")
-    public TrainRest updateTrain(@PathVariable String id, @RequestBody TrainDetailsModel trainDetails) {
-        TrainRest trainRest = new TrainRest();
-        TrainDTO trainDTO = new TrainDTO();
-        BeanUtils.copyProperties(trainDetails, trainDTO);
-        trainDTO = trainService.updateTrain(id, trainDTO);
-        BeanUtils.copyProperties(trainDTO, trainRest);
-        return trainRest;
-    }
+//    @PutMapping(path = "/{id}")
+//    public TrainRest updateTrain(@PathVariable String id, @RequestBody TrainDetailsModel trainDetails) {
+//        TrainRest trainRest = new TrainRest();
+//        TrainDTO trainDTO = new TrainDTO();
+//        BeanUtils.copyProperties(trainDetails, trainDTO);
+//        trainDTO = trainService.updateTrain(id, trainDTO);
+//        BeanUtils.copyProperties(trainDTO, trainRest);
+//        return trainRest;
+//    }
 
     @DeleteMapping(path = "/{id}")
     public OperationStatus deleteTrain(@PathVariable String id) {
