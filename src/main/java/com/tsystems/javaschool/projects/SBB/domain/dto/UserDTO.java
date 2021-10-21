783dd76806c332be1c330fcc3b314a5ef146b795
vgrigory <grigoryevvadim@mail.ru>
2021-10-22 00:27:13 +0300

    back to start page link is added

--- src/main/java/com/tsystems/javaschool/projects/SBB/domain/dto/UserDTO.java
+++ src/main/java/com/tsystems/javaschool/projects/SBB/domain/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.tsystems.javaschool.projects.SBB.domain.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -14,5 +15,6 @@ public class UserDTO implements Serializable {
     private String firstname;
     private String lastname;
     private String email;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 }
