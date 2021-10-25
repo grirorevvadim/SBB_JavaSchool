8039eddc52fba79c4013b15cd38e63d352befbcc
vgrigory <grigoryevvadim@mail.ru>
2021-10-25 13:47:10 +0300

    annotation Data changed by Getter/Setter for Schedule

--- src/main/java/com/tsystems/javaschool/projects/SBB/domain/entity/Schedule.java
+++ src/main/java/com/tsystems/javaschool/projects/SBB/domain/entity/Schedule.java
@@ -1,6 +1,8 @@
 package com.tsystems.javaschool.projects.SBB.domain.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,7 +11,8 @@ import javax.persistence.ManyToOne;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity(name = "schedules")
 public class Schedule extends AbstractEntity implements Serializable {
     private static final long serialVersionID = 63427531L;
